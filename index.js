5d88919018a0a3e9b52acf2150fbeed0a6cd025e
Fire <84128345+Fireisbest@users.noreply.github.com>
2021-09-04 22:17:58 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const { Discord, Client, Collection, MessageEmbed, Intents } = require('discord.js');
+const client = new Client({ intents: ['GUILDS', 'GUILD_MEMBERS', 'GUILD_MESSAGES', 'GUILD_INVITES']});
+const config = require('./config.json');
+client.on('ready', () => {
+  console.log(`${client.user.tag} is Online!`)
+})
+client.login(config.token)
